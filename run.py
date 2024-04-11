febea836b64d46370971195f7e5ce9db19c377fa
oliverke-tech <oliverke.tech@gmail.com>
2024-04-11 11:14:18 -0400

    Final version

--- run.py
+++ run.py
@@ -3,6 +3,5 @@ from app import app
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 app.config['SECRET_KEY'] = 'my_secret_key'
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5050', debug=True)
